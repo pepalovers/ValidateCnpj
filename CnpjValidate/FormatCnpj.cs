01a4bba0b17e7a0a77f5ce043df3f36fef9c6a65
Pietro Ramos de Almeida Pacheco <pietro.pacheco@engecorps.com.br>
2024-07-25 16:39:29 -0300

    Funcionando

---
+++ CnpjValidate/FormatCnpj.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CnpjVerify
+{
+
+    public class Validation
+    {
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Write the CNPJ:");
+        //    string cnpj = Console.ReadLine();
+        //    bool IsValid = Check(cnpj);
+        //    Console.WriteLine(IsValid);
+
+        //    if (IsValid == !false)
+        //    {
+        //        string formatedCnpj = Format(cnpj);
+        //        Console.WriteLine(formatedCnpj);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Invalid CNPJ");
+        //    }
+        //}
+
+        public bool CheckTrue(string cnpj)
+        {
+            string cleanedCnpj = Regex.Replace(cnpj, @"[^\d]", "");
+            return cleanedCnpj.Length == 14;
+        }
+
+        public string CompleteCnpj(string cnpj)
+        {
+            Validation validation = new Validation();
+            bool validate = validation.CheckTrue(cnpj);
+            if (validate == true)
+            {
+                string cleanedCnpj = Regex.Replace(cnpj, @"[^\d]", "");
+                return cleanedCnpj;
+            }
+            else
+            {
+                return "CNPJ Invalido";
+            }
+        }
+        public string Format(string cnpj)
+        {
+
+            string cleanToFormat = Regex.Replace(cnpj, @"[^\d]", "");
+
+            cleanToFormat = cleanToFormat.Insert(2, ".");
+            cleanToFormat = cleanToFormat.Insert(6, ".");
+            cleanToFormat = cleanToFormat.Insert(10, "/");
+            cleanToFormat = cleanToFormat.Insert(15, "-");
+
+            return cleanToFormat;
+        }
+
+
+    }
+}
---
+++ CnpjValidate/Program.cs
@@ -0,0 +1,52 @@
+﻿using ApiRequestWs;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+using CnpjValidate;
+using CnpjVerify;
+
+
+namespace MainProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validation validation = new Validation();
+            Console.WriteLine("Write the CNPJ:");
+            var cnpj = Console.ReadLine();
+            bool IsValid = validation.CheckTrue(cnpj);
+            string finalCnpj = validation.CompleteCnpj(cnpj);
+            Console.WriteLine(finalCnpj);
+
+            if (IsValid == !false)
+            {
+                teste(finalCnpj);
+            }
+            else
+            {
+                Console.WriteLine("Invalid CNPJ");
+            }
+
+            
+
+
+        }
+        async static void teste(string cnpj)
+        {
+            try
+            {
+                var empresaBrasil = await DadosEmpresaWs.GetEmpresa(cnpj);
+                Console.WriteLine(empresaBrasil.capital_social);
+                Console.WriteLine(empresaBrasil.razao_social);
+            }
+            catch (Exception ex)
+            {
+                throw(ex);
+
+            }
+
+        }
+    }
+}
